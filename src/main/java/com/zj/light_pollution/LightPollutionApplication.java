4cdf8feb9dfa063de179e40be6785c7fde34ea64
Jun Zhang <offwhite1124@gmail.com>
2019-01-05 14:08:17 +0800

    Create project

---
+++ src/main/java/com/zj/light_pollution/LightPollutionApplication.java
@@ -0,0 +1,14 @@
+package com.zj.light_pollution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightPollutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightPollutionApplication.class, args);
+    }
+
+}
+
